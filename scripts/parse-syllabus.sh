2703f4cbb8ca776117f9458133024d654ce6a893
Sarah Grefalda <sarahrhgrefalda@gmail.com>
2016-11-19 14:21:37 -0800

    fixing script, adding sample (entity.json)

--- scripts/parse-syllabus.sh
+++ scripts/parse-syllabus.sh
@@ -1,1 +1,1 @@
-curl -s -k -H "Content-Type: application/json" -H "Authorization: Bearer @YOUR-ACCESS-TOKEN" https://language.googleapis.com/v1/documents:analyzeEntities -d @../entity.json > result.json
+curl -s -k -H "Content-Type: application/json" -H "Authorization: Bearer ya29.El-bA_NRFAHK_J-rjS7Z0hExSaqd0Z7Nq0KZKAdeoV1AaS164cInACgVutmkU_Sqxg13eURRKZSlGUyc7FvNL-BzcuBymCGxoyzTisNCFYFMuGf7DtBQU5U8PTVODk2XSQ" https://language.googleapis.com/v1/documents:analyzeEntities -d @entity.json > result.json
